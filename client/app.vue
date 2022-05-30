1641b932bf13e5e4bbec8ad3439943b1e2b29a16
naby <naby@pop-os.localdomain>
2022-05-30 09:45:49 +0200

    template

---
+++ client/app.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <p class="bg-green-900 text-white">
+      {{ authors.data.authors.data[0].attributes.name }}
+    </p>
+    <NuxtWelcome />
+  </div>
+</template>
+<script setup lang="ts">
+import { authorQuery } from '~/graphql/query'
+const graphql = useStrapiGraphQL()
+
+const authors = await graphql(authorQuery)
+</script>
